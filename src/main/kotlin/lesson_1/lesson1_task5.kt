fd89c2135bc34f8bbae9fd4314d37c112f57f840
mlinnik <mlinnik@yoomoney.ru>
2025-05-08 15:27:35 +0300

    Правки после ревью

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,17 +1,14 @@
 package org.example.lesson_1
 
-import java.lang.String.format
-
 private const val SECONDS_PER_HOUR = 3600
 private const val SECONDS_PER_MINUTE = 60
 
 fun main() {
     val totalSeconds: Int = 6480
 
-    // Переводим количество секунд в целые минуты и целые часы, сохраняя остаток секунд и минут в новые переменные
     val fullHours: Int = totalSeconds / SECONDS_PER_HOUR
     val fullMinutes: Int = (totalSeconds % SECONDS_PER_HOUR) / SECONDS_PER_MINUTE
     val fullSeconds: Int = totalSeconds % SECONDS_PER_MINUTE
 
-    println(format("%02d:%02d:%02d", fullHours, fullMinutes, fullSeconds))
+    println("%02d:%02d:%02d".format(fullHours, fullMinutes, fullSeconds))
 }
